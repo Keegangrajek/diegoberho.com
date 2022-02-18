92a9d5ff2a127aaa23920014762d596fc6fe1c30
KeeganGrajek <Keegangrajek@gmail.com>
2022-02-18 09:35:53 -0800

    Just a commit to establish changes
    I am going to make a testing branch, and I will never merge it. This is purely for testing...

---
+++ diego-website/website-files/javascript-files/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+});
+
+function headerLinkContactHoverOn() {
+    document.getElementsByClassName('header-link-contact')[0].style.color = "red"
+};
+
+function headerLinkContactHoverOff() {
+    document.getElementsByClassName('header-link-contact')[0].style.color = "black";
+}
