dbf003c95f8f40007fd63cd096e5e481a7eb75d3
KeeganGrajek <Keegangrajek@gmail.com>
2022-04-12 00:49:42 -0700

    trying to upload properly this time
    test to see if upload works properly. Code wasn't working.

--- live-website/website-files/javascript-files/index.js
+++ live-website/website-files/javascript-files/index.js
@@ -17,6 +17,32 @@ function displayOutsideBoxToggle() {
     }
 }
 
+var previousNum1 = "default";
+var previousNum2 = "default";
+
+function switchPicture(num1, num2) {
+    document.getElementsByClassName("picture-box")[num1].classList.toggle("picture-"+num2);
+    document.getElementsByClassName("a-link-" + num1 + "" + num2 )[0].classList.add("photo-selection-clicked-button");
+    console.log("num1: ("+ num1 +")");
+    console.log("num2: ("+ num2 +")");
+    console.log("num1 and num2:" + num1 + "" + num2);
+    removeOldPicture(previousNum1, previousNum2);
+    previousNum1 = num1;
+    previousNum2 = num2;
+}
+
+function removeOldPicture(num1, num2) {
+    if (previousNum1 == "default"){
+        document.getElementsByClassName("picture-box")[0].classList.toggle("picture-0");
+    } else {
+        document.getElementsByClassName("picture-box")[num1].classList.toggle("picture-"+num2);
+    } if (previousNum2 == "default") {
+        document.getElementsByClassName("a-link-00")[0].classList.remove("photo-selection-clicked-button");
+    } else {
+        document.getElementsByClassName("a-link-"+num1+num2)[0].classList.remove("photo-selection-clicked-button");
+    }
+    
+}
 
 /* // This doesn't work right now I will try and fix this.
 var element = document.getElementsByClassName('go-back-button');
