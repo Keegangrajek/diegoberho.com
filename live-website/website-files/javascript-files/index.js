b7d0962e8bd0d373bd1648c796df9f0249567062
KeeganGrajek <Keegangrajek@gmail.com>
2022-08-12 00:02:00 -0700

    saving commit
    Just saving this to the hub just in case. Changes that have been made so far are of the new "studies page". Still not done with it but some organization progress has been made.

--- live-website/website-files/javascript-files/index.js
+++ live-website/website-files/javascript-files/index.js
@@ -55,6 +55,7 @@ function toggleMobileNavbar() {
     var box2 = document.getElementsByClassName('body-box-1');
     var box3 = document.getElementsByClassName('body-about-box-1');
     var box4 = document.getElementsByClassName('body-about-box-10');
+    var notifyBar = document.getElementsByClassName('notify-bar');
     try {
         if (!mobileNavbarState) {
             console.log("uh-oh2");
@@ -80,6 +81,11 @@ function toggleMobileNavbar() {
             try {
                 box4[1].style.setProperty('display', 'none');
             } catch { console.log("box4sub")}
+            try {
+                notifyBar[0].style.setProperty('display', 'none');
+            } catch {
+                console.log("notify-bar");
+            }
             return;
         } 
         if (mobileNavbarState) {
@@ -106,6 +112,11 @@ function toggleMobileNavbar() {
             try {
                 box4[1].style.setProperty('display', 'flex');
             } catch { console.log("box4sub")}
+            try {
+                notifyBar[0].style.setProperty('display', 'flex');
+            } catch {
+                console.log("notify-bar");
+            }
             return;
         }
     } catch {
