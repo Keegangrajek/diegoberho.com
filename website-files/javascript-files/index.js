0bf9abfe373aab23f1d51a86879f9e8f31417678
KeeganGrajek <Keegangrajek@gmail.com>
2022-08-27 08:49:49 -0700

    Commit to test folder structure and issues
    I think some links will be destroyed but I don't know so I will just revert changes if there are any issues.

---
+++ website-files/javascript-files/index.js
@@ -0,0 +1,202 @@
+const navbarHoverEffects = false;
+
+var elementOneGroupContact;
+var elementTwoGroupContact;
+var mobileNavbarState; // Open = true; Closed = false;
+
+document.addEventListener('DOMContentLoaded', () => {  
+    elementOneGroupContact = document.getElementsByClassName("header-link-div-contact");
+    elementTwoGroupContact = document.getElementsByClassName("header-link-contact-dropdown");
+    mobileNavbarState = false; // Open = true; Closed = false;
+});
+
+function displayOutsideBoxToggle() {
+    if(navbarHoverEffects){
+        elementOneGroupContact[0].classList.toggle("hover-box-contact-switch-off");
+        elementOneGroupContact[1].classList.toggle("hover-box-contact-switch-off");
+        elementTwoGroupContact[0].classList.toggle("header-link-contact-dropdown-display");
+        elementTwoGroupContact[0].classList.toggle("header-link-contact-dropdown-display-not");
+    }
+}
+
+function purchase1PaintingImageSelection(num1, num2){
+    var elm = document.getElementsByClassName("bb51b-b-" + num1);
+    var elm2 = document.getElementsByClassName("a1-link-b-"+num1+ "" +num2);
+    var elm3 = document.getElementsByClassName("bb51b-picture-"+num1);
+    for(index = 0; index < elm[0].children.length; index++){
+        document.getElementsByClassName("a1-link-b-"+num1+ "" +index)[0].classList.remove("photo-selection-clicked-button");
+    }
+    for(index = 0; index < elm[0].children.length; index++){
+        elm3[0].classList.remove("ps1-picture-"+num1+""+index);
+    }
+    elm2[0].classList.add("photo-selection-clicked-button");
+    elm3[0].classList.add("ps1-picture-"+num1+""+num2);
+    return;
+}
+
+function purchasePaintingImageSelection(num1, num2){
+    var elm = document.getElementsByClassName("bb5b-b-" + num1);
+    var elm2 = document.getElementsByClassName("a-link-b-"+num1+ "" +num2);
+    var elm3 = document.getElementsByClassName("bb5b-picture-"+num1);
+    for(index = 0; index < elm[0].children.length; index++){
+        document.getElementsByClassName("a-link-b-"+num1+ "" +index)[0].classList.remove("photo-selection-clicked-button");
+    }
+    for(index = 0; index < elm[0].children.length; index++){
+        elm3[0].classList.remove("ps-picture-"+num1+""+index);
+    }
+    elm2[0].classList.add("photo-selection-clicked-button");
+    elm3[0].classList.add("ps-picture-"+num1+""+num2);
+    return;
+}
+
+function purchasePaintingImageSelection2(num1, num2){
+    var elm = document.getElementsByClassName("bb6b-b-" + num1);
+    var elm2 = document.getElementsByClassName("b-link-b-"+num1+ "" +num2);
+    var elm3 = document.getElementsByClassName("bb6b-picture-"+num1);
+    for(index = 0; index < elm[0].children.length; index++){
+        document.getElementsByClassName("b-link-b-"+num1+ "" +index)[0].classList.remove("photo-selection-clicked-button");
+    }
+    for(index = 0; index < elm[0].children.length; index++){
+        elm3[0].classList.remove("pss-picture-"+num1+""+index);
+    }
+    elm2[0].classList.add("photo-selection-clicked-button");
+    elm3[0].classList.add("pss-picture-"+num1+""+num2);
+    return;
+}
+
+function originalsPaintingImageSelection(num1, num2){
+    var elm = document.getElementsByClassName("o-bb5b-" + num1);
+    var elm2 = document.getElementsByClassName("a-link-"+num1+ "" +num2);
+    var elm3 = document.getElementsByClassName("o-picture-"+num1);
+    try {
+        for(index = 0; index < elm[0].children.length; index++){
+            document.getElementsByClassName("a-link-"+num1+ "" +index)[0].classList.remove("photo-selection-clicked-button");
+        }
+        for(index = 0; index < elm[0].children.length; index++){
+            elm3[0].classList.remove("o-picture-"+num1+""+index);
+        }
+    } catch { console.log("error");}
+    elm2[0].classList.add("photo-selection-clicked-button");
+    elm3[0].classList.add("o-picture-"+num1+""+num2);
+    return;
+}
+
+function originals1PaintingImageSelection(num1, num2){
+    var elm = document.getElementsByClassName("o-bb51b-" + num1);
+    var elm2 = document.getElementsByClassName("a1-link-"+num1+ "" +num2);
+    var elm3 = document.getElementsByClassName("o1-picture-"+num1);
+    try {
+        for(index = 0; index < elm[0].children.length; index++){
+            document.getElementsByClassName("a1-link-"+num1+ "" +index)[0].classList.remove("photo-selection-clicked-button");
+        }
+        for(index = 0; index < elm[0].children.length; index++){
+            elm3[0].classList.remove("o1-picture-"+num1+""+index);
+        }
+    } catch { console.log("error");}
+    elm2[0].classList.add("photo-selection-clicked-button");
+    elm3[0].classList.add("o1-picture-"+num1+""+num2);
+    return;
+}
+
+function studiesPaintingImageSelection(num1, num2){
+    var elm = document.getElementsByClassName("s-bb6b-" + num1);
+    var elm2 = document.getElementsByClassName("b-link-"+num1+ "" +num2);
+    var elm3 = document.getElementsByClassName("s-picture-"+num1);
+    try {
+        for(index = 0; index < elm[0].children.length; index++){
+            document.getElementsByClassName("b-link-"+num1+ "" +index)[0].classList.remove("photo-selection-clicked-button");
+        }
+        for(index = 0; index < elm[0].children.length; index++){
+            elm3[0].classList.remove("s-picture-"+num1+""+index);
+        }
+    } catch {
+       console.log("children-error"); 
+    }
+    elm2[0].classList.add("photo-selection-clicked-button");
+    elm3[0].classList.add("s-picture-"+num1+""+num2);
+    return;
+}
+
+function toggleMobileNavbar() {
+    var elm = document.getElementsByClassName("header-link-mobilemenu-dropdown");
+    var box = document.getElementsByClassName('body-box-5');
+    var box2 = document.getElementsByClassName('body-box-1');
+    var box3 = document.getElementsByClassName('body-about-box-1');
+    var box4 = document.getElementsByClassName('body-about-box-10');
+    var notifyBar = document.getElementsByClassName('notify-bar');
+    try {
+        if (!mobileNavbarState) {
+            console.log("uh-oh2");
+            console.log(mobileNavbarState);
+            elm[0].classList.toggle("header-link-mobilemenu-dropdown-display-not");
+            elm[0].classList.toggle("header-link-mobilemen-dropdown-display-yes");
+            document.getElementById('span-1').className = 'mobile-menu-bars-span-animation-1';
+            document.getElementById('span-2').className = 'mobile-menu-bars-span-animation-2';
+            document.getElementById('span-3').className = 'mobile-menu-bars-span-animation-3';
+            mobileNavbarState = true;
+            try {
+                box[0].style.setProperty('display', 'none');
+            } catch { console.log("box")}
+            try {
+                box2[0].style.setProperty('display', 'none');
+            } catch { console.log("box2")}
+            try {
+                box3[0].style.setProperty('display', 'none');
+            } catch { console.log("box3")}
+            try {
+                box4[0].style.setProperty('display', 'none');
+            } catch { console.log("box4")}
+            try {
+                box4[1].style.setProperty('display', 'none');
+            } catch { console.log("box4sub")}
+            try {
+                notifyBar[0].style.setProperty('display', 'none');
+            } catch {
+                console.log("notify-bar");
+            }
+            return;
+        } 
+        if (mobileNavbarState) {
+            console.log("uh-oh1");
+            console.log(mobileNavbarState);
+            elm[0].classList.toggle("header-link-mobilemenu-dropdown-display-not");
+            elm[0].classList.toggle("header-link-mobilemenu-dropdown-display-yes");
+            document.getElementById('span-1').className = 'mobile-menu-bars-span-animation-1-not';
+            document.getElementById('span-2').className = 'mobile-menu-bars-span-animation-2-not';
+            document.getElementById('span-3').className = 'mobile-menu-bars-span-animation-3-not';
+            mobileNavbarState = false;
+            try {
+                box[0].style.setProperty('display', 'flex');
+            } catch { console.log("box")}
+            try { 
+                box2[0].style.setProperty('display', 'flex');
+            } catch { console.log("box2")}
+            try {
+                box3[0].style.setProperty('display', 'flex');
+            } catch { console.log("box3")}
+            try {
+                box4[0].style.setProperty('display', 'flex');
+            } catch { console.log("box4")}
+            try {
+                box4[1].style.setProperty('display', 'flex');
+            } catch { console.log("box4sub")}
+            try {
+                notifyBar[0].style.setProperty('display', 'flex');
+            } catch {
+                console.log("notify-bar");
+            }
+            return;
+        }
+    } catch {
+        console.log("error");
+    }
+}
+
+/* // This doesn't work right now I will try and fix this.
+var element = document.getElementsByClassName('go-back-button');
+element[0].setAttribute('href', document.referrer);
+
+element.onclick = function() {
+    window.history.go(-1);
+    return false;
+} */
